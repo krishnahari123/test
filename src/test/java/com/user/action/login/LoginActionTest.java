6f1896dd457fc0a37470272f3cf8acbdbcad3eb3
krishnahari123 <59728787+krishnahari123@users.noreply.github.com>
2020-03-03 17:29:48 +0530

    Update LoginActionTest.java

--- src/test/java/com/user/action/login/LoginActionTest.java
+++ src/test/java/com/user/action/login/LoginActionTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "classpath:features/login/"}, glue = "com.user.action.login")
+@CucumberOptions(features = { "classpath:features/login/"}, glue = "com.user.action.login" ,plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" })
 public class LoginActionTest {
 
 }
